83983cf2e9d6571c6c7bce72a88da3f6d03aa4fa
Ai-riku <116927374+Ai-riku@users.noreply.github.com>
2023-11-12 19:19:32 -0800

    minor code adjustments

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from dotenv import load_dotenv
 from IPython.display import display, Audio
 from PIL import Image
 from moviepy.editor import VideoFileClip, AudioFileClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from openai import OpenAI
 
 import cv2
 import base64
 import time
-from openai import OpenAI
-
 import io
 import os
 import requests
+import tempfile
 
 import streamlit as st
-import tempfile
 import numpy as np
 
 load_dotenv()
@@ -64,6 +64,51 @@ def frames_to_story(base64Frames, prompt):
     print(result.choices[0].message.content)
     return result.choices[0].message.content
 
+# Text to audio
+def text_to_audio(text):
+    response = requests.post(
+        "https://api.openai.com/v1/audio/speech",
+        headers={
+            "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+        },
+        json={
+            "model": "tts-1",
+            "input": text,
+            "voice": "onyx",
+        },
+    )
+
+    if response.status_code != 200:
+        raise Exception("Request failed with status code")
+
+    # Save audio to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as tmpfile:
+        for chunk in response.iter_content(chunk_size=1024 * 1024):
+            tmpfile.write(chunk)
+        audio_filename = tmpfile.name
+
+    return audio_filename
+
+# Merge audio and video
+def merge_audio_video(video_filename, audio_filename, output_filename):
+    print("Merging audio and video...")
+    print("Video filename:", video_filename)
+    print("Audio filename:", audio_filename)
+
+    video_clip = VideoFileClip(video_filename)
+    audio_clip = AudioFileClip(audio_filename)
+    final_clip = video_clip.set_audio(audio_clip)
+
+    # Write the result to a file (without audio)
+    final_clip.write_videofile(
+        output_filename, codec='libx264', audio_codec='aac')
+
+    # Close the clips
+    video_clip.close()
+    audio_clip.close()
+
+    return output_filename
+
 
 # Streamlit UI
 def main():
@@ -73,9 +118,8 @@ def main():
     uploaded_file = st.file_uploader("Choose a file")
 
     if uploaded_file is not None:
-        st.video(uploaded_file)
         prompt = st.text_area(
-            "Prompt", value="These are frames of a quick product demo walkthrough. Create a short voiceover script that outline the key actions to take, that can be used along this product demo.")
+            "Prompt", value="Provide exciting commentary!")
 
     if st.button('Generate', type="primary") and uploaded_file is not None:
         with st.spinner('Processing...'):
@@ -85,27 +129,21 @@ def main():
             est_word_count = video_duration * 2
             final_prompt = prompt + f"(This video is ONLY {video_duration} seconds long, so make sure the voice over MUST be able to be explained in less than {est_word_count} words)"
 
-            # st.write(final_prompt)
             text = frames_to_story(base64Frames, final_prompt)
             st.write(text)
 
-            # # Generate audio from text
-            # audio_filename, audio_bytes_io = text_to_audio(text)
+            audio_filename = text_to_audio(text)
+            output_video_filename = os.path.splitext(video_filename)[
+                0] + '_output.mp4'
+            final_video_filename = merge_audio_video(
+                video_filename, audio_filename, output_video_filename)
 
-            # # Merge audio and video
-            # output_video_filename = os.path.splitext(video_filename)[
-            #     0] + '_output.mp4'
-            # final_video_filename = merge_audio_video(
-            #     video_filename, audio_filename, output_video_filename)
+            st.video(final_video_filename)
 
-            # # Display the result
-            # st.video(final_video_filename)
-
-            # # Clean up the temporary files
-            # os.unlink(video_filename)
-            # os.unlink(audio_filename)
-            # os.unlink(final_video_filename)
-
+            # Clean up the temporary files
+            os.unlink(video_filename)
+            os.unlink(audio_filename)
+            os.unlink(final_video_filename)
 
 if __name__ == '__main__':
     main()
